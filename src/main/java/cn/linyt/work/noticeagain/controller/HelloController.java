79d70fb8abefe30f0c3ada662e1287d5b0fee845
Mojo <1316578052@qq.com>
2020-04-08 23:47:20 +0800

    1

--- src/main/java/cn/linyt/work/noticeagain/controller/HelloController.java
+++ src/main/java/cn/linyt/work/noticeagain/controller/HelloController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class HelloController {
 
+    @RequestMapping
     public String hello() {
 
         return "hello,world!!!";

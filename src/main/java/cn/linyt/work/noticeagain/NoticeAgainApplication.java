3cfb813ea19aea519031560ba44b6a5e57c16d67
Mojo <1316578052@qq.com>
2020-04-08 23:22:17 +0800

    1

---
+++ src/main/java/cn/linyt/work/noticeagain/NoticeAgainApplication.java
@@ -0,0 +1,13 @@
+package cn.linyt.work.noticeagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeAgainApplication.class, args);
+    }
+
+}
---
+++ src/main/java/cn/linyt/work/noticeagain/controller/HelloController.java
@@ -0,0 +1,21 @@
+package cn.linyt.work.noticeagain.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author Mojo
+ * @Date 2020/4/8 23:07
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping
+public class HelloController {
+
+    public String hello() {
+
+        return "hello,world!!!";
+    }
+}
